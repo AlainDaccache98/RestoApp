7b3be2b2ad3f811a77ec708dd5880606aa814742
ksr987 <rampurkarketan@gmail.com>
2018-03-01 19:54:28 -0500

    added new files

---
+++ src/ca/mcgill/ecse223/resto/application/RestoAppApplication.java
@@ -0,0 +1,33 @@
+package ca.mcgill.ecse223.resto.application;
+
+import ca.mcgill.ecse223.resto.model.RestoApp;
+import ca.mcgill.ecse223.resto.view.RestoAppPage;
+
+
+public class RestoAppApplication {
+
+private static RestoApp resto;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// start UI
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+            	new RestoAppPage().setVisible(true);
+            }
+        });
+        
+	}
+
+	public static RestoApp getRestoApp() {
+		if (resto == null) {
+			// load model
+			// TODO
+			// for now, we are just creating an empty BTMS
+			resto = new RestoApp();
+		}
+ 		return resto;
+	}
+}
---
+++ src/ca/mcgill/ecse223/resto/controller/Controller.java
@@ -0,0 +1,86 @@
+package ca.mcgill.ecse223.resto.controller;
+
+import java.util.List;
+
+import ca.mcgill.ecse223.resto.application.RestoAppApplication;
+import ca.mcgill.ecse223.resto.model.Order;
+import ca.mcgill.ecse223.resto.model.RestoApp;
+import ca.mcgill.ecse223.resto.model.Seat;
+import ca.mcgill.ecse223.resto.model.Table;
+
+public class Controller {
+	
+	public static void createTable(int number, int x, int y, int width, int length, int numberOfSeats) throws InvalidInputException {
+		RestoApp r = RestoAppApplication.getRestoApp();
+
+		List<Table> currentTables = r.getCurrentTables();
+				
+		for( Table currentTable : currentTables){
+			if(currentTable.doesOverlap(x, y, width, length)){
+				throw new InvalidInputException("Överlapped");
+			}
+		}
+		
+		Table table = new Table(number, x,y, width, length, r);
+		r.addCurrentTable(table);
+		for(int i =0; i< numberOfSeats; i++){
+			Seat seat = table.addSeat();
+			table.addCurrentSeat(seat);
+		}
+
+	}
+	
+	public static void updateTable(Table table, int newNumber, int numberOfSeats ) throws InvalidInputException{
+		
+		//update table number
+		if(table==null || newNumber < 0 || numberOfSeats<0){
+			throw new InvalidInputException("Invalid arguments");
+		}
+		
+		boolean reserved = table.hasReservations();
+		
+		if(reserved==true){
+			throw new InvalidInputException("Table already reserved");
+		}
+		
+		
+		RestoApp r = RestoAppApplication.getRestoApp();
+		
+		List<Order> currentOrders = r.getCurrentOrders();
+		
+		for(Order order: currentOrders){
+			List<Table> tables = order.getTables();
+			boolean inUse = tables.contains(table);
+			
+			if(inUse==true){
+				throw new InvalidInputException("Table already in use");
+
+			}
+		}
+		
+		try{
+			table.setNumber(newNumber);
+		}
+		catch(RuntimeException e){
+			throw new InvalidInputException("New table number has already been assigned to a table");
+		}
+		
+		//update number of seats
+		
+		//add seat
+		int n = table.numberOfCurrentSeats();
+		
+		for(int i=0; i<(numberOfSeats-n);i++){
+			Seat seat = table.addSeat();
+			table.addCurrentSeat(seat);
+		}
+		
+		//remove seat
+		for(int i=1; i<(n-numberOfSeats);i++){
+			Seat seat = table.getCurrentSeat(0);
+			table.removeCurrentSeat(seat);
+		}			
+		
+	}		
+
+}
